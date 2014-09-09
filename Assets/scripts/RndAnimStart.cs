4a2a6251598e783c7df0dd6620952ee7a77dd8bb
WilckersonGanda <wilckersonganda@MacBook-de-Wilckerson.local>
2014-09-09 19:09:03 -0300

    Scroll

---
+++ Assets/scripts/RndAnimStart.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RndAnimStart : MonoBehaviour {
+
+	public float range = 10;
+	float rndTime = 0;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	
+		animator = GetComponent<Animator>();
+		animator.speed = 0;
+		rndTime = Time.time + Random.Range(1,range);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(rndTime != 0 && Time.time > rndTime)
+		{
+			animator.speed = 1;
+			rndTime = 0;
+		}
+	}
+}
---
+++ Assets/scripts/UIScroll.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScroll : MonoBehaviour
+{
+
+	bool pressed = false;
+	Vector3 dragDelta;
+	Vector3 mouseLastPosition;
+
+	public GameObject items;
+	public GameObject top;
+	public GameObject bottom;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		updateDragDelta();
+
+		var itemPos = items.transform.position;
+		itemPos.y += dragDelta.y * Time.deltaTime;
+		items.transform.position = itemPos;
+
+		foreach (Transform listItem in items.transform) {
+
+			if(listItem.position.y > top.transform.position.y
+			   || listItem.position.y < bottom.transform.position.y)
+			{
+				listItem.gameObject.SetActive(false);
+			}
+			else
+			{
+				listItem.gameObject.SetActive(true);
+			}
+		}
+	}
+
+	void updateDragDelta(){
+
+		if (Input.GetMouseButtonDown (0)) {
+			pressed = true;
+			mouseLastPosition = Input.mousePosition;
+			dragDelta = Vector3.zero;
+		} else if (Input.GetMouseButtonUp (0)) {
+			pressed = false;
+			dragDelta = Vector3.zero;
+		}
+		
+		if (pressed) {
+			
+			//Debug.Log (Input.mousePosition);
+			dragDelta = Input.mousePosition - mouseLastPosition;
+			Debug.Log(dragDelta);
+			
+			mouseLastPosition = Input.mousePosition;
+		}
+	}
+}
