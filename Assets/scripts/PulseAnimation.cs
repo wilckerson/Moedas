2b639a95ad5ae2b28225460c73c00c687faec482
WilckersonGanda <wilckersonganda@MacBook-de-Wilckerson.local>
2014-09-12 14:47:38 -0300

    Aplicando UI

---
+++ Assets/scripts/PulseAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulseAnimation : MonoBehaviour {
+
+	public float velocity = 4;//4;
+	public float pulse = 0.03f;//70;
+
+	Vector3 initialScale;
+	float rndX = 0;
+	float rndY = 0;
+	// Use this for initialization
+	void Start () {
+		initialScale = transform.localScale;
+
+		rndX = Random.Range(-1,1f);
+		rndY = Random.Range(-1,1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		var modifX = 1 - (Mathf.Sin((rndX + Time.time) * velocity) / pulse);
+//		var modifY = 1 - (Mathf.Cos((rndY + Time.time) * velocity) / pulse);
+//
+//		transform.localScale = new Vector3(
+//			initialScale.x * modifX,
+//			initialScale.y * modifY,
+//			initialScale.z);
+
+		var modifX = Mathf.Sign(Mathf.Sin((rndX + Time.time) * velocity)) * pulse * Time.deltaTime;
+		var modifY = Mathf.Sign(Mathf.Sin((rndY + Time.time) * velocity)) * pulse * Time.deltaTime;
+		transform.localScale += new Vector3(modifX,modifY,0);
+//		transform.localScale = new Vector3(
+//			transform.localScale.x * modif,
+//			transform.localScale.y * modif,
+//			transform.localScale.z);
+
+
+	}
+}
--- Assets/scripts/UI/UIButtom.cs
+++ Assets/scripts/UI/UIButtom.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class UIButtom : MonoBehaviour
 {
 
-	public Sprite activeSprite;
-	private Sprite currentSprite;
-	private SpriteRenderer spriteRenderer;
+	//public Sprite activeSprite;
+	//private Sprite currentSprite;
+	//private SpriteRenderer spriteRenderer;
+	public float scaleFactor = 1.1f;
 	private bool pressed = false;
 
 	// Use this for initialization
 	void Start ()
 	{
-		spriteRenderer = GetComponent<SpriteRenderer> ();
-		currentSprite = spriteRenderer.sprite;
+		//spriteRenderer = GetComponent<SpriteRenderer> ();
+		//currentSprite = spriteRenderer.sprite;
 	}
 	
 	// Update is called once per frame
@@ -23,14 +24,26 @@ public class UIButtom : MonoBehaviour
 
 	void OnMouseDown ()
 	{
+		Debug.Log("Down");
 		pressed = true;
-		spriteRenderer.sprite = activeSprite;
+		transform.localScale = new Vector3(
+			transform.localScale.x * scaleFactor,
+			transform.localScale.y * scaleFactor,
+			transform.localScale.z
+			);
+		//spriteRenderer.sprite = activeSprite;
 	}
 
 	void OnMouseUp ()
 	{
+		Debug.Log("Up");
 		pressed = false;
-		spriteRenderer.sprite = currentSprite;
+		transform.localScale = new Vector3(
+			transform.localScale.x / scaleFactor,
+			transform.localScale.y / scaleFactor,
+			transform.localScale.z
+			);
+		//spriteRenderer.sprite = currentSprite;
 	}
 
 	public bool IsPressed(){
